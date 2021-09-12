dd2583d12bbfb02d3e6425bb11df87b091826103
NicolasDestito <nicolasdestito74@gmail.com>
2021-09-12 19:59:27 -0300

    Adicionado IFs de login

--- principal/views.py
+++ principal/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
-
+is_logged_in = False
 # TODO: Criar os htmls para cada view
 
 def index(request):
@@ -17,28 +16,56 @@ def explorar(request):
     return render(request, "principal/explorar.html")
 
 def visualizar(request):
-    return render(request, "principal/visualizar.html")
+
+    if is_logged_in:
+        return render(request, "principal/index.html")
+    elif not is_logged_in:
+        return redirect('login')
 
 def usuario(request):
-    return render(request, "principal/usuario.html")
+    if is_logged_in:
+        return render(request, "principal/usuario.html")
+    elif not is_logged_in:
+        return redirect('login')
 
 def editarconta(request):
-    return render(request, "principal/editarconta.html")
+    if is_logged_in:
+        return render(request, "principal/editarconta.html")
+    elif not is_logged_in:
+        return redirect('login')
 
 def socorrosmeus(request):
-    return render(request, "principal/socorrosmeus.html")
+    if is_logged_in:
+        return render(request, "principal/socorrosmeus.html")
+    elif not is_logged_in:
+        return redirect('login')
 
 def criacao(request):
-    return render(request, "principal/criacao.html")
+    if is_logged_in:
+        return render(request, "principal/criacao.html")
+    elif not is_logged_in:
+        return redirect('login')
 
 def criar(request):
+    
     return redirect('socorrosmeus')
 
 def editar(request):
-    return render(request, "principal/criacao.html") #Usa o html de criação, mas iremos carregar as informações junto
+    if is_logged_in:
+        return render(request, "principal/criacao.html") #Usa o html de criação, mas iremos carregar as informações junto
+    elif not is_logged_in:
+        return redirect('login')
 
 def atualizar(request):
-    return redirect('socorrosmeus')
+    if is_logged_in:
+        # Apenas fazer a ação de atualizar aqui dentro.
+        return redirect('socorrosmeus')
+    elif not is_logged_in:
+        return redirect('login')
 
 def deletar(request):
-    return redirect('socorrosmeus')
+    if is_logged_in:
+        # Apenas fazer a ação de deletar aqui dentro.
+        return redirect('socorrosmeus')
+    elif not is_logged_in:
+        return redirect('login')
