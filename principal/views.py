b0c5c978a33725154eef6ca75f12d2f99749f9bb
NicolasDestito <nicolasdestito74@gmail.com>
2021-09-12 00:43:30 -0300

    Novos comentarios e views.py com htmls corretos

--- principal/views.py
+++ principal/views.py
@@ -2,38 +2,40 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
+# TODO: Criar os htmls para cada view
+
 def index(request):
     return render(request, "principal/index.html")
 
 def registro(request):
-    return render(request, "principal/index.html")
+    return render(request, "principal/registro.html")
 
 def login(request):
-    return render(request, "principal/index.html")
+    return render(request, "principal/login.html")
 
 def explorar(request):
-    return render(request, "principal/index.html")
+    return render(request, "principal/explorar.html")
 
 def visualizar(request):
-    return render(request, "principal/index.html")
+    return render(request, "principal/visualizar.html")
 
 def usuario(request):
-    return render(request, "principal/index.html")
+    return render(request, "principal/usuario.html")
 
 def editarconta(request):
-    return render(request, "principal/index.html")
+    return render(request, "principal/editarconta.html")
 
 def socorrosmeus(request):
-    return render(request, "principal/index.html")
+    return render(request, "principal/socorrosmeus.html")
 
 def criacao(request):
-    return render(request, "principal/index.html")
+    return render(request, "principal/criacao.html")
 
 def criar(request):
     return redirect('socorrosmeus')
 
 def editar(request):
-    return render(request, "principal/index.html") #Usa o html de criação, mas iremos carregar as informações junto
+    return render(request, "principal/criacao.html") #Usa o html de criação, mas iremos carregar as informações junto
 
 def atualizar(request):
     return redirect('socorrosmeus')
