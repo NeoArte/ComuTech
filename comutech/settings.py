ba501cc44a71681fc5ad6d3299ed8c3347356798
Entra21 <lipebehling@gmail.com>
2021-09-14 16:13:03 -0300

    Criou pasta templates

--- comutech/settings.py
+++ comutech/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'comutech.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, )],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
