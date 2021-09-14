d7d0695aa647bef1e37f7024537856d87071b3ba
NicolasDestito <nicolasdestito74@gmail.com>
2021-09-14 10:50:46 -0300

    Criado o model para Usuario

--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+import django.core.validators as validators
+# import datetime
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    cpf = models.IntegerField(max_length=11, validators=[validators.MinLengthValidator(11)]) # 11 números compõem um CPF, pode ser usado como primeiro campo de um login
+    telefone = models.IntegerField(max_length=13)
+    cep = models.IntegerField(max_length=8, validators=[validators.MinLengthValidator(8)])
+    data_nascimento = models.DateTimeField() # Recbe um objeto datetime que conterá as informações da data. ex: dt = datetime(2015, 10, 09, 23, 55, 59, 25454...)
+    senha = models.CharField(max_length=255)
+
+    whatsapp = models.IntegerField(max_length=13)
+    twitter = models.CharField(blank=True, max_length=15, validators=[validators.MinLengthValidator(4)]) # Isso se refere ao @ da pessoa, todavia o @ (character) não entra.
+    facebook = models.CharField(blank=True)
+    instagram = models.CharField(blank=True, max_length=30) # Isso se referece ao nome de úsuario (@), não o nome.
+
+    data_criacao = models.DateTimeField(default=timezone.now)
