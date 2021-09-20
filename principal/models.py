8d59838e8661f76c975515bfea699cfd98295f3f
NicolasDestito <nicolasdestito74@gmail.com>
2021-09-20 11:49:47 -0300

    User manager criado

--- principal/models.py
+++ principal/models.py
@@ -1,10 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils import timezone
 import django.core.validators as validators
 # import datetime
 
 
-class User(models.Model): # Usuários
+# Classes ligadas ao usuário (User related classes) ===================
+
+# User Manager é a classe responsável por lidar com o que ocorre durante a criação de um usuario (create_user) e super usuario (create_super_user)
+class UserManager(BaseUserManager):
+    def create_user(self, cpf, name, email, phone, cep, password=None):
+        if not cpf:
+            raise ValueError("Usuários precisam ter um CPF - Users must have a CPF")
+        
+        if not cpf:
+            raise ValueError("Usuários precisam ter um nome - Users must have a name")
+
+        if not email:
+            raise ValueError("Usuários precisam ter um endereço de email - Users must have an email adress")
+
+        if not phone:
+            raise ValueError("Usuários precisam ter um número de celular - Users must have a phone number")
+        
+        if not cep:
+            raise ValueError("Usuários precisam ter um CEP - Users must have a CEP")
+        
+        user = self.model(
+            cpf=cpf,
+            name=name,
+            email=self.normalize_email(email), # Normalize -> deixa tudo em letra minúscula
+            phone=phone,
+            cep=cep
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+
+class User(AbstractBaseUser): # Usuários
 
     # Define um úsuario dentro da plataforma. Esse possui: nome, email, cpf (apenas números), telefone, cep (apenas números), 
     # data de nascimento (s/ horário, formato padrão é AAAA-MM-DD-hh-mm-ss-ms, Ano-Mês-Dia-Hora-Minuto-Segundo-Milisegundo), senha,
@@ -12,8 +47,8 @@ class User(models.Model): # Usuários
     # twitter (nome de úsuario sem o @), facebook (nome de perfil) e instagram (nome de úsuario)
 
     name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    cpf = models.CharField(max_length=11, validators=[validators.MinLengthValidator(11)])
+    email = models.EmailField(max_length=255, unique=True)
+    cpf = models.CharField(max_length=11, validators=[validators.MinLengthValidator(11)], unique=True)
     phone = models.CharField(max_length=13)
     cep = models.CharField(max_length=8, validators=[validators.MinLengthValidator(8)])
     birth_date = models.DateField()
@@ -25,7 +60,32 @@ class User(models.Model): # Usuários
     facebook = models.CharField(blank=True, max_length=255)
     instagram = models.CharField(blank=True, max_length=30)
 
-    creation_date = models.DateTimeField(default=timezone.now)
+
+    # OBRIGADO pelo Django (REQUIRED by Django)
+
+    date_joined = models.DateTimeField(auto_now_add=timezone.now, verbose_name="date joined")
+    last_login = models.DateTimeField(auto_now=True, verbose_name="last login")
+    is_admin = models.BooleanField(default=False) # É admin?
+    is_active = models.BooleanField(default=True) # Está ativo?
+    is_staff = models.BooleanField(default=False) # É da equipe (quem trabalha)?
+    is_superuser = models.BooleanField(default=False) # É um super usuário (privilégios especiais)
+
+    USERNAME_FIELD = 'cpf' # O que será usado para realizar o login
+    REQUIRED_FIELDS = ['name', 'email', 'phone', 'cep']
+
+    # O que retornar ao dar print em User
+    def __str__(self):
+        return self.name + ":" + self.email+ ":" + self.cpf
+        
+    # Retorna se o usuario tem permissão (é admin)
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perm(self, app_label):
+        return True
+
+
+# Classes Comuns (Common Classes) ========================
 
 
 
