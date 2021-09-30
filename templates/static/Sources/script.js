643892bb1f58d753b4fffd3d3fe1c9e43eca4d88
Arthur-Net0 <arthuriddle@gmail.com>
2021-09-30 07:38:39 -0300

    Image render ja esta funcionanado

--- templates/static/Sources/script.js
+++ templates/static/Sources/script.js
@@ -31,3 +31,34 @@ function socialNetworks() {
     let twitter = document.getElementById("input-twitter")
 }
 socialNetworks()
+
+//File load section
+
+/*
+document.getElementById('imagem').onchange = function(event) {
+    let image = event.target.files[0] // Esta linha pega um determinado arquivo do input
+    let rend = new FileReader() //Cria um objeto com uma classe com acesso a funções de leitura de arquivos
+    rend.readAsDataURL(image)
+
+    rend.onload = function(){ //Função que determinada um bloco de código ao carregar um arquivo
+        let imageRender = document.getElementById('output') //Pega o objeto html em que a imagem vai ser renderizado
+        imageRender.src = this.result //Atribui a rota de renderização criada pela função .readAsDataURL()
+      }
+}*/
+
+//Register File Load 
+
+
+document.getElementById('_inputIMG').onchange = function(event) {
+    let image = event.target.files[0] // Esta linha pega um determinado arquivo do input
+    let render = new FileReader() //Cria um objeto com uma classe com acesso a funções de leitura de arquivos
+    render.readAsDataURL(image)
+
+    render.onload = function(){ //Função que determinada um bloco de código ao carregar um arquivo
+        let imageRender = document.getElementById('img-render') //Pega o objeto html em que a imagem vai ser renderizado
+        let renderContent = document.getElementById('img-render-content')
+        imageRender.src = this.result //Atribui a rota de renderização criada pela função .readAsDataURL()
+        renderContent.style.display = 'initial';
+        document.getElementById('file-img').style.display = 'none';
+      }
+}
