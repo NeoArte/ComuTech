c489e5be6be1ca217d63d6c372577b96abd1dd86
NicolasDestito <nicolasdestito74@gmail.com>
2021-10-11 16:55:13 -0300

    aid_checker com e-mails funcionando

--- principal/tasks.py
+++ principal/tasks.py
@@ -25,28 +25,29 @@ def aid_checker():
     print("\n\n\nRODANDO AID_CHECKER\n\n\n")
     aid_list  = Aid.objects.all()
     for aid in aid_list:
-
+        print(f"\n {aid} : {aid.id} : {aid.title}")
         pause_time = aid.creation_date + timedelta(days=60)
         delete_time = aid.creation_date + timedelta(days=74)
 
-        #print("Aid ID: ", aid.id, " | ","Today: ", timezone.now(), " | ", "PT: ", pause_time, " | ", "DT: ", delete_time, end="")
-        print(f'\n\n{aid}:{aid.id}:{type(aid)}\n\n')
+        print("Aid ID: ", aid.id, " | ","Today: ", timezone.now(), " | ", "PT: ", pause_time, " | ", "DT: ", delete_time, end="")
         if type(aid) != type(None):
+            print("Not None", end="")
             if timezone.now() >= pause_time and aid.state != "C":
+                print("CONGELA")
                 send = send_mail(
                     subject= 'Teste Nome',
                     message= f'Olá, {aid.author.name}!\n',
                     from_email= settings.EMAIL_HOST_USER,
-                    recipient_list= [''],
+                    recipient_list= [aid.author.email],
                 )
                 if send:
                     print(" | Sent")
                 else:
                     print(" | Not Sent")
-                print(f'\n\n{aid}:{aid.id}:{type(aid)}\n\n')
                 aid.state = "C"
-                aid = aid.save()
+                aid.save()
             
             if aid.state == "C" and timezone.now() >= delete_time:
                 print("\n\n\nDeletando\n\n\n")
                 aid.delete()
+    print("\nTERMINANDO AID_CHECKER\n")
