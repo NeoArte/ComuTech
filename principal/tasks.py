89745af2386eab827f01a58cb122d7de5ed255cb
NicolasDestito <nicolasdestito74@gmail.com>
2021-10-10 21:42:43 -0300

    Email funcionando, bug com NoneType

--- principal/tasks.py
+++ principal/tasks.py
@@ -1,6 +1,22 @@
 from .models import Aid
 from datetime import time, timedelta
 from django.utils import timezone
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Enviar email - testes
+
+def email_send():
+    enviou = send_mail(
+    'Teste',
+    'Mensagem de Teste',
+    settings.EMAIL_HOST_USER,
+    ['artemisaneo@protonmail.com'],
+)
+    if enviou:
+        print('\n\n\nEnviado\n\n\n')
+    else:
+        print('\n\n\nNão enviou\n\n\n')
 
 
 # Para iniciar use: python manage.py qcluster
@@ -13,11 +29,24 @@ def aid_checker():
         pause_time = aid.creation_date + timedelta(days=60)
         delete_time = aid.creation_date + timedelta(days=74)
 
-        print("Aid ID: ", aid.id, " | ","Today: ", timezone.now(), " | ", "PT: ", pause_time, " | ", "DT: ", delete_time)
-
-        if timezone.now() >= pause_time and aid.state is not "C":
-            aid.state = "C"
-            aid = aid.save()
-        
-        if aid.state == "C" and timezone.now() >= delete_time:
-            aid.delete()
+        #print("Aid ID: ", aid.id, " | ","Today: ", timezone.now(), " | ", "PT: ", pause_time, " | ", "DT: ", delete_time, end="")
+        print(f'\n\n{aid}:{aid.id}:{type(aid)}\n\n')
+        if type(aid) != type(None):
+            if timezone.now() >= pause_time and aid.state != "C":
+                send = send_mail(
+                    subject= 'Teste Nome',
+                    message= f'Olá, {aid.author.name}!\n',
+                    from_email= settings.EMAIL_HOST_USER,
+                    recipient_list= [''],
+                )
+                if send:
+                    print(" | Sent")
+                else:
+                    print(" | Not Sent")
+                print(f'\n\n{aid}:{aid.id}:{type(aid)}\n\n')
+                aid.state = "C"
+                aid = aid.save()
+            
+            if aid.state == "C" and timezone.now() >= delete_time:
+                print("\n\n\nDeletando\n\n\n")
+                aid.delete()
--- principal/tests.py
+++ principal/tests.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
-from datetime import time, timedelta, date
 
 # Create your tests here.
-print(date.today() < date.today() + timedelta(days=5))
+print(
+        f"Olá, Zé!\n" \
+        "Esse é um e-mail automático do sistema da Comutech para te informar que o seu socorro, 'Ajuda com móveis', foi pausado e não aparecerá " \
+        "em nosso sistema. Em duas semanas ele será removido da nossa base de dados, caso você deseje continuar a postagem acesse a sua " \
+        "página em 127.0.0.1:8000/explorar/2 e clique em opções ou na página do seu perfil em 127.0.0.1:8000/usuarios/1 " \
+        "selecione 'opções' no cartão do seu socorro, nas opções então selecione 'descongelar'."
+)
