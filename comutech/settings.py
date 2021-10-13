aa93640e320e4407abd222508f01863c81ff219c
Marciann0 <marciannoooo0@outlook.com>
2021-10-13 17:18:30 -0300

    Medidas de seguranças feitas

--- comutech/settings.py
+++ comutech/settings.py
@@ -13,7 +13,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
+import environ
 
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_i*!i%ku=+j28dos*dpka(rn$)5(xk%opj4ll=8kuzk@*w7ot&'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
