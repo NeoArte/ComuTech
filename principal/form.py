03a848f8a5ed8d3649838b9e54def6d150d483ea
Arthur-Net0 <arthuriddle@gmail.com>
2021-10-17 21:36:22 -0300

    Mascaras full implementadas e funcionais

--- principal/form.py
+++ principal/form.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm, fields, widgets
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import Review, User, Aid, AidPhotos
 from django.contrib.admin import widgets
-
+import re
 
 class RegistrationForm(UserCreationForm):
     name = forms.CharField(max_length=255, help_text="Seu nome")
@@ -149,14 +149,14 @@ class RegistrationForm(UserCreationForm):
         user = super(RegistrationForm, self).save(commit=False)
         user.name = self.cleaned_data['name']
         user.email = self.cleaned_data['email']
-        user.phone = self.cleaned_data['phone']
-        user.cpf = self.cleaned_data['cpf']
-        user.cep = self.cleaned_data['cep']
+        user.phone = re.sub('\D', '', self.cleaned_data['phone']).replace(' ', '')
+        user.cpf = re.sub('\D', '', self.cleaned_data['cpf']).replace(' ', '')
+        user.cep = re.sub('\D', '', self.cleaned_data['cep']).replace(' ', '')
         user.birth_date = self.cleaned_data['birth_date']
 
         #ITENS OPCIONAIS
         user.facebook = self.cleaned_data['facebook']
-        user.whatsapp = self.cleaned_data['whatsapp']
+        user.whatsapp = re.sub('\D', '', self.cleaned_data['whatsapp']).replace(' ', '')
         user.twitter = self.cleaned_data['twitter']
         user.instagram = self.cleaned_data['facebook']
         user.profile_picture = self.cleaned_data['profile_picture']
@@ -171,7 +171,6 @@ class RegistrationForm(UserCreationForm):
         if commit:
             user.save()
         return user
-
 
 class EditProfileForm(UserChangeForm):
     
@@ -294,7 +293,33 @@ class EditProfileForm(UserChangeForm):
             'placeholder':'Informações adicionais',
             'input-valid':'true'
         })
-        
+    def save(self, commit=True):
+        user = super(EditProfileForm, self).save(commit=False)
+        user.name = self.cleaned_data['name']
+        user.email = self.cleaned_data['email']
+        user.phone = re.sub('\D', '', self.cleaned_data['phone']).replace(' ', '')
+        user.cpf = re.sub('\D', '', self.cleaned_data['cpf']).replace(' ', '')
+        user.cep = re.sub('\D', '', self.cleaned_data['cep']).replace(' ', '')
+        user.birth_date = self.cleaned_data['birth_date']
+
+        #ITENS OPCIONAIS
+        user.facebook = self.cleaned_data['facebook']
+        user.whatsapp = re.sub('\D', '', self.cleaned_data['whatsapp']).replace(' ', '')
+        user.twitter = self.cleaned_data['twitter']
+        user.instagram = self.cleaned_data['facebook']
+        user.profile_picture = self.cleaned_data['profile_picture']
+        user.country = "Brasil"
+        user.state = self.cleaned_data['state']
+        user.city = self.cleaned_data['city']
+        user.neighborhood = self.cleaned_data['neighborhood']
+        user.street = self.cleaned_data['street']
+        user.house_number = self.cleaned_data['house_number']
+        user.add_info = self.cleaned_data['add_info']
+
+        if commit:
+            user.save()
+        return user
+    
 
 
 class AidForm(ModelForm):
